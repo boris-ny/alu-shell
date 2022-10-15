f6255472e863ac556599ec2d83cc10e5777940a8
boris-ny <b.nyilindek@alustudent.com>
2022-10-15 12:41:53 +0200

    date and time🔔

---
+++ test.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+# This script is to print the current date and time in the format of "YYYY-MM-DD HH:MM:SS"
+
+# Get the current date and time
+current_date_time=$(date "+%Y-%m-%d %H:%M:%S")
+echo "Current date and time: $current_date_time"
